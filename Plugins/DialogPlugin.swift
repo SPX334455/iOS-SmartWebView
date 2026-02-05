06c37fea1ec036ee165cf6194c0259845ae26535
SPX334455 <119264181+SPX334455@users.noreply.github.com>
2026-02-05 23:15:26 +0300

    Create DialogPlugin.swift

---
+++ Plugins/DialogPlugin.swift
@@ -0,0 +1,14 @@
+import Foundation
+import WebKit
+import UIKit
+
+final class DialogPlugin: NSObject {
+
+    static let shared = DialogPlugin()
+
+    private override init() {
+        super.init()
+    }
+
+    // Placeholder – alert vs. sonradan eklenir
+}
