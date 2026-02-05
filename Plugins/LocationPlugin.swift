da0e31e89658f5bc629ba3976b46b6c3bb8e75e6
SPX334455 <119264181+SPX334455@users.noreply.github.com>
2026-02-05 23:15:03 +0300

    Create LocationPlugin.swift

---
+++ Plugins/LocationPlugin.swift
@@ -0,0 +1,14 @@
+import Foundation
+import WebKit
+import CoreLocation
+
+final class LocationPlugin: NSObject {
+
+    static let shared = LocationPlugin()
+
+    private override init() {
+        super.init()
+    }
+
+    // Placeholder – ileride eklersin
+}
